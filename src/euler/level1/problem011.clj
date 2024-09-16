b72e5e7412eda875d402531374b101f1e1e5a59f
Josh Zakaras <joshzakaras@gmail.com>
2024-09-16 14:07:04 -0700

    redid euler-11 with proper TDD

--- src/euler/level1/problem011.clj
+++ src/euler/level1/problem011.clj
@@ -1,51 +1,61 @@
 (ns euler.level1.problem011
-  (:require [clojure.string :as str]))
+  (:require
+    [clojure.string :as str]))
 
-(defn formatGrid [grid]
-  (for [row (map #(str/split % #" ") grid)]
-    (map #(Integer/valueOf %) row)))
+(defn row-to-integers [row] (map #(Integer/valueOf %) row))
 
-(defn calculateGridDimensions [grid]
-  {:x (count (first grid)) :y (count grid)})
+(defn string-to-2d-vector [string]
+  (->> (str/split string #"\n")
+       (map #(str/split % #" "))))
 
-(defn getColumns [grid]
-  (let [gridDimensions (calculateGridDimensions (formatGrid grid))]
-    (for [y (range 0 (get gridDimensions :y))]
-      (take-nth (get gridDimensions :y) (drop y (flatten (formatGrid grid)))))))
+(defn format-grid [string]
+  (if (empty? string)
+    []
+    (->> (string-to-2d-vector string)
+         (map #(row-to-integers %)))))
 
-(defn getRows [grid]
-  (let [gridDimensions (calculateGridDimensions (formatGrid grid))]
-    (->> (formatGrid grid)
-         flatten
-         (partition (get gridDimensions :x)))))
+(defn count-grid-columns [grid]
+  (count (first grid)))
 
-(defn getLeftDiagonals [grid]
-  (let [formattedGrid (formatGrid grid)]
-    (let [gridDimensions (calculateGridDimensions formattedGrid)]
-      (concat
-        (for [row (reverse (range 1 (inc (get gridDimensions :y))))]
-          (->> (range row)
-               (map #(nth (nth formattedGrid %) (Math/abs (- % (dec row)))))))
-        (for [column (reverse (range 1 (get gridDimensions :x)))]
-          (->> (range column)
-               (map #(nth (nth formattedGrid (- (dec (get gridDimensions :x)) (Math/abs (- % (dec column))))) (- (dec (get gridDimensions :x)) %)))))))))
+(defn columns-from-grid [grid]
+  (if (empty? grid)
+    []
+    (->> (range (count-grid-columns grid))
+         (map #(take-nth (count-grid-columns grid) (drop % (flatten grid)))))))
 
-(defn getRightDiagonals [grid]
-  (let [formattedGrid (map reverse (formatGrid grid))]
-    (let [gridDimensions (calculateGridDimensions formattedGrid)]
-      (concat
-        (for [row (reverse (range 1 (inc (get gridDimensions :y))))]
-          (->> (range row)
-               (map #(nth (nth formattedGrid %) (Math/abs (- % (dec row)))))))
-        (for [column (reverse (range 1 (get gridDimensions :x)))]
-          (->> (range column)
-               (map #(nth (nth formattedGrid (- (dec (get gridDimensions :x)) (Math/abs (- % (dec column))))) (- (dec (get gridDimensions :x)) %)))))))))
+(defn format-diagonal [unformatted-diagonal]
+  (->> (reverse unformatted-diagonal)
+       (map-indexed vector)
+       reverse
+       (map #(nth (last %) (first %)))))
 
-(defn greatestProductInSet [n set]
-  (->> (mapcat #(partition n 1 %) set)
-       (map #(reduce * %))
-       (apply max)))
+(defn second-half-of-diagonals [grid]
+  (->> (map reverse grid)
+       reverse
+       (reductions conj [])
+       rest
+       (map format-diagonal)
+       (drop-last)
+       (map reverse)
+       reverse))
 
-(defn euler-11 [n grid]
-  (->> (map #(greatestProductInSet n %) [(getRows grid) (getColumns grid) (getLeftDiagonals grid) (getRightDiagonals grid)])
-       (apply max)))
+(defn left-diagonals-from-grid [grid]
+  (concat
+    (map format-diagonal (rest (reductions conj [] grid)))
+    (second-half-of-diagonals grid)
+    ))
+
+(defn right-diagonals-from-grid [grid]
+  (->> (left-diagonals-from-grid (reverse grid))
+       (map reverse)
+       reverse))
+
+(defn get-n-adjacent-from-set [n set]
+  (mapcat #(partition n 1 %) set))
+
+(defn euler-11 [n string]
+  (let [grid (format-grid string)]
+    (->> (conj [] grid (columns-from-grid grid) (left-diagonals-from-grid grid) (right-diagonals-from-grid grid))
+         (mapcat #(get-n-adjacent-from-set n %))
+         (map #(apply * %))
+         (apply max))))
